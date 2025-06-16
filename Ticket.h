7ff0e4c46c8184a94138d07a694651e0d358c4dc
GeryPan <gpandelieva@gmail.com>
2025-06-16 22:35:02 +0300

    Ticket.h

--- Ticket.h
+++ Ticket.h
@@ -11,7 +11,7 @@ public:
     Ticket(Movie* new_movie, unsigned new_row, unsigned new_col);
     Ticket(const Ticket& other);
     Ticket& operator=(const Ticket& other);
-    Movie* Movie() const;
+    Movie* get_movie() const;
     unsigned Row() const;
     unsigned Col() const;
     bool is_expired(const char* current_date) const;
